d4c4d2231724c0961a2f47c83b4cb3bb42dbad66
unknown <[anubhavgupta2260@gmail.com]>
2020-06-27 14:03:42 +0530

    initial commit

---
+++ _pages/Books.md
@@ -0,0 +1,22 @@
+---
+layout: page
+title: Books
+permalink: /Books/
+---
+<body>
+  <div class="container">
+    <div class="image">
+      <img src="images\02.jpg" alt="Rani Tatt">
+      <h3>Rani Tatt</h3>
+      <h3>250</h3>
+      <a class ="add-cart cart1" href ="#">Add Cart</a>
+    </div>
+    <div class="image">
+      <img src="images\03.jpg" alt="Rani Tatt">
+      <h3>T shirt</h3>
+      <h3>2500</h3>
+      <a class ="add-cart cart2" href ="#">Add Cart</a>
+    </div>
+
+  <script src="{{ site.baseurl }}/js/main.js"></script>
+</body>
---
+++ js/main.js
@@ -0,0 +1,64 @@
+let carts = document.querySelectorAll('a.add-cart');
+let products = [
+    {
+        name :'Rani Tatt@',
+        tag : 'Rani Tatt@',
+        price: 250,
+        inCart : 0
+    },
+    {
+        name :'Rani Tatt-2',
+        tag : 'Rani Tatt-2',
+        price: 2500,
+        inCart : 0   
+    }
+]
+for (let i=0;i<carts.length;i++){
+   
+    carts[i].addEventListener('click',()=>{
+        cartNumbers(products[i]);
+
+
+    })
+
+}
+function cartNumbers(product){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    setItems(product);
+    productNumbers = parseInt(productNumbers);
+    if(productNumbers ){
+    localStorage.setItem('cartNumbers',productNumbers+1);
+    console.log("in pn");
+
+    //document.querySelector('.nav_item span').textContent=productNumbers+1;
+    }
+     else{
+        localStorage.setItem('cartNumbers',1);
+      //  document.querySelector('.nav__item span').textContent=1; not working 
+
+    }
+ 
+      
+
+}
+ 
+function setItems(product){
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if(cartItems != null){
+        if(cartItems[product.tag] == undefined){
+            cartItems ={
+                ...cartItems,
+                [product.tag]:product
+            }}
+            cartItems[product.tag].inCart +=1;
+        }
+    else{
+            product.inCart =1;
+            cartItems ={
+                [product.tag]:product
+            }
+    }
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
